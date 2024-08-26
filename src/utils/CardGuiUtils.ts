01d71afe23528a2e3229a09145ceb5e9cb9bebbc
MisterFlask <ironlordbyron@gmail.com>
2024-08-26 10:32:33 -0500

    improved tooltip+card name appearance +desc appearance

--- src/utils/CardGuiUtils.ts
+++ src/utils/CardGuiUtils.ts
@@ -11,7 +11,7 @@ export interface GameConfig {
 }
 
 export class CardGuiUtils {
- config: GameConfig = {
+    config: GameConfig = {
         cardWidth: 120,
         cardHeight: 160,
         battlefieldY: 200,
@@ -30,17 +30,45 @@ export class CardGuiUtils {
         const cardImage = scene.add.image(0, -cardHeight / 4, cardTexture)
             .setDisplaySize(cardWidth / 2, cardHeight / 2);
 
-        const nameBackground = scene.add.rectangle(0, cardHeight / 4, cardWidth - 10, 30, 0xffffff).setStrokeStyle(2, 0x000000);
+        const nameBackground = scene.add.rectangle(0, cardHeight / 4, cardWidth - 10, 60, 0xffffff).setStrokeStyle(2, 0x000000);
         const nameText = scene.add.text(0, cardHeight / 4, data.name, { fontSize: '16px', color: '#000', wordWrap: { width: cardWidth - 10 } });
-        const descText = scene.add.text(0, cardHeight / 2, data.description, { fontSize: '12px', color: '#000', wordWrap: { width: cardWidth - 10 } });
-        const tooltipText = scene.add.text(cardWidth + 5, 0, data.tooltip || '', { 
-            fontSize: '12px', 
-            color: '#000', 
+
+        const descText = scene.add.text(0, cardHeight / 2, data.description, {
+            fontSize: '12px',
+            color: '#000',
+            wordWrap: { width: cardWidth - 20 },
+            align: 'center'
+        }).setVisible(false);
+
+        const tooltipText = scene.add.text(cardWidth + 5, 0, data.tooltip || '', {
+            fontSize: '12px',
+            color: '#000',
             wordWrap: { width: cardWidth - 20 },
             align: 'left'
-        });
-        const infoBackground = scene.add.rectangle(0, cardHeight / 2, cardWidth - 10, 60, 0xffffff).setVisible(false).setStrokeStyle(2, 0x000000);
-        const tooltipBackground = scene.add.rectangle(cardWidth + cardWidth / 2, 0, cardWidth - 10, cardHeight, 0xffffff).setVisible(false).setStrokeStyle(2, 0x000000);
+        }).setVisible(false);
+
+        // Create backgrounds that will resize with the text
+        const infoBackground = scene.add.rectangle(0, cardHeight / 2, cardWidth - 10, 60, 0xffffff).setStrokeStyle(2, 0x000000);
+        const tooltipBackground = scene.add.rectangle(cardWidth + cardWidth / 2, 0, cardWidth - 10, cardHeight, 0xffffff).setStrokeStyle(2, 0x000000);
+
+        // Function to update background sizes
+        const updateBackgrounds = () => {
+            infoBackground.setSize(cardWidth - 10, descText.height + 20);
+            infoBackground.setPosition(0, cardHeight / 2 + descText.height / 4);
+            descText.setPosition(0, cardHeight / 2 + descText.height / 4);
+            // Update name background and text
+            nameBackground.setSize(cardWidth - 10, nameText.height + 10);
+            nameBackground.setPosition(0, cardHeight / 4);
+            nameText.setPosition(0, cardHeight / 4);
+
+            tooltipBackground.setSize(cardWidth - 10, tooltipText.height + 20);
+            tooltipBackground.setPosition(cardWidth + cardWidth / 2, tooltipText.height / 2);
+        };
+
+        // Call updateBackgrounds initially
+        updateBackgrounds();
+        descText.on('changedata', updateBackgrounds);
+        tooltipText.on('changedata', updateBackgrounds);
 
         // Create hidden highlight border
         const highlightBorder = scene.add.rectangle(0, 0, cardWidth + 10, cardHeight + 10, 0xffff00)
@@ -56,6 +84,9 @@ export class CardGuiUtils {
         descText.setVisible(false);
         tooltipText.setVisible(false);
 
+        infoBackground.setVisible(false);
+        tooltipBackground.setVisible(false);
+        
         cardContainer.add([cardBackground, cardImage, nameBackground, nameText, tooltipBackground, tooltipText, infoBackground, descText, highlightBorder]);
         cardContainer.setSize(cardWidth, cardHeight);
         cardContainer.setInteractive(new Phaser.Geom.Rectangle(0, 0, cardWidth, cardHeight), Phaser.Geom.Rectangle.Contains);
